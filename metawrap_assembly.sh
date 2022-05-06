fd633770ea1349bf52e39ad7182b2bc34d1c73bf
yenjh3910 <82366605+yenjh3910@users.noreply.github.com>
2022-05-07 02:49:18 +0800

    Update metawrap_assembly.sh

--- metawrap_assembly.sh
+++ metawrap_assembly.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-for i in $(<~/sample_list/clean_read_list)
-do
+#metawrap_coassembly
+gunzip ~/clean_read/*.gz
+rm ~/clean_read/*.fastq
+cat ~/clean_read/*_1.fastq > ~/clean_read/all_reads_1.fastq
+cat ~/clean_read/*_2.fastq > ~/clean_read/all_reads_2.fastq
+metawrap assembly -1 ~/clean_read/all_reads_1.fastq -2 ~/clean_read/all_reads_2.fastq -m 56 -t 16 --megahit -o ~/metawrap_coassembly
 
-done
+#quast
+quast.py ~/metawrap_coassembly/final.contigs.fa -o coassembly_quast
