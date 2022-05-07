fa970e82bad7babc6b425c0a1dd6022f9bd8f9cc
yenjh3910 <82366605+yenjh3910@users.noreply.github.com>
2022-05-07 22:43:17 +0800

    Update metawrap_binning_refinement_quant.sh

--- metawrap_binning_refinement_quant.sh
+++ metawrap_binning_refinement_quant.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
 
+#binning
+metawrap binning -o S1S3_initial_binning -t 16 -a ~/megahit/S1S3_megehit/S1S3_final.contigs.fa --maxbin2 --concoct *fastq
+
+#refinement
+metawrap bin_refinement -o S1S3_bin_refinement -t 16 -A metabat2_bins -B maxbin2_bins -C  concoct_bins -c 50 -x 5 -m 0.95
+
+#Quant_bins
