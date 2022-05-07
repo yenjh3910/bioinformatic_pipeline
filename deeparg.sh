7021c652a2fe9875047a50473c5132d3a05b1cfc
yenjh3910 <82366605+yenjh3910@users.noreply.github.com>
2022-05-07 13:43:47 +0800

    Create deeparg.sh

---
+++ deeparg.sh
@@ -0,0 +1,1 @@
+deeparg short_reads_pipeline --forward_pe_file ~/clean_read/${i}_1.fastq.gz --reverse_pe_file ~/clean_read/${i}_2.fastq.gz --output_file ~/deeparg/${i}_deeparg -d ~/deeparg_download --bowtie_16s_identity 0.85
