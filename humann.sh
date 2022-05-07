cce2ac2716965ac4ca89f61deff8546d4cc835ca
yenjh3910 <82366605+yenjh3910@users.noreply.github.com>
2022-05-07 17:32:27 +0800

    Update humann.sh

--- humann.sh
+++ humann.sh
@@ -3,4 +3,24 @@
 for i in $(<~/sample_list/clean_read_list)
 do
 
-cat ~/clean_read/_1.fastq.gz  ~/clean_read/S1_clean_R2.fastq.gz > merge_S1.fastq.gz
+cat ~/clean_read/${i}_1.fastq.gz  ~/clean_read/${i}_2.fastq.gz > ~/clean_read/${i}.fastq.gz
+
+#humann
+##run
+humann -i ~/clean_read/${i}.fastq.gz -o ~/humann --input-format fastq.gz --threads 16
+
+rm ~/clean_read/${i}.fastq.gz
+
+done
+
+##Join the output files
+humann_join_tables --input ~/humann --output ~/humann/all_genefamilies.tsv --file_name genefamilies
+humann_join_tables --input ~/humann --output ~/humann/all_pathabundance.tsv --file_name pathabundance
+humann_join_tables --input ~/humann --output ~/humann/all_pathcoverage.tsv --file_name pathcoverage
+
+##humann_rename_table
+humann_rename_table --input ~/humann/all_genefamilies.tsv --names uniref90 --output ~/humann/all_genefamilies_rename_uniref90.tsv
+
+##Renormalization
+humann_renorm_table --input ~/humann/all_pathabundance.tsv --output ~/humann/all_pathabundance_cpm.tsv  --units cpm
+humann_renorm_table --input ~/humann/all_pathabundance.tsv --output ~/humann/all_pathabundance_relab.tsv  --units relab
