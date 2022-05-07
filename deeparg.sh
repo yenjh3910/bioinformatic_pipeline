1fb68225d375fdc9e8953501b548394b61226757
yenjh3910 <82366605+yenjh3910@users.noreply.github.com>
2022-05-07 16:03:20 +0800

    Update deeparg.sh

--- deeparg.sh
+++ deeparg.sh
@@ -5,5 +5,7 @@ do
 
 #deeparg
 deeparg short_reads_pipeline --forward_pe_file ~/clean_read/${i}_1.fastq.gz --reverse_pe_file ~/clean_read/${i}_2.fastq.gz --output_file ~/deeparg/${i}_deeparg -d ~/deeparg_download --bowtie_16s_identity 0.85
+rm *paired
+rm *merged
 
 done
