a77ddba02b13328e060184992bf00f7000105b18
yenjh3910 <82366605+yenjh3910@users.noreply.github.com>
2022-05-08 11:18:39 +0800

    Update metawrap_binning_refinement_quant.sh

--- metawrap_binning_refinement_quant.sh
+++ metawrap_binning_refinement_quant.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 #binning
-metawrap binning -o initial_binning -t 16 -a ~/megahit/S1S3_megehit/S1S3_final.contigs.fa --maxbin2 --concoct *fastq
+metawrap binning -o ~/metawrap_run/initial_binning -t 16 -a ~/megahit/megehit_coassembly/final.contigs.fa --maxbin2 --concoct *fastq
 
 #refinement
-metawrap bin_refinement -o S1S3_bin_refinement -t 16 -A metabat2_bins -B maxbin2_bins -C  concoct_bins -c 50 -x 5 -m 0.95
+metawrap bin_refinement -o ~/metawrap_run/bin_refinement -t 16 -A metabat2_bins -B maxbin2_bins -C  concoct_bins -c 50 -x 10 -m 0.95
 
 #Quant_bins
+metawrap quant_bins -b ~/metawrap_run/bin_refinement/metawrap_50_10_bins -o ~/metawrap_run/bin_quant -a ~/megahit/S1S3_megehit/S1S3_final.contigs.fa ~/data_download/*fastq -t 16
