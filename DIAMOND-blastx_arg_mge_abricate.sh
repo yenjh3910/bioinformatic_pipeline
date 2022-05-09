8845118a310096c5788b34370ccd88c92acc7187
yenjh3910 <82366605+yenjh3910@users.noreply.github.com>
2022-05-09 20:17:41 +0800

    Create DIAMOND-blastx_arg_mge_abricate.sh

---
+++ DIAMOND-blastx_arg_mge_abricate.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mkdir MGEs
+mkdir ARGs
+
+for i in $(<~/sample_list/clean_read_list)
+do
+
+#MGEs detection
+diamond blastx -d ~/Database/MGEs90.dmnd -q ~/cdhit/bin.${i}.nucl.uniq --id 50 -p 16 -e 1e-7 -k 1 --query-cover 50 -o ~/MGEs/bin.${i}.nucl.uniq.MGEs90.dmnd
+
+#ARG-ARGminer detection
+diamond blastx -d ~/ARGminer/ARGminer.dmnd -q ~/cdhit/bin.$i.nucl.uniq --id 50 -p 16 -e 1e-7 -k 1 --query-cover 50 -o ~/ARGs/bin.${i}.nucl.uniq.ARGminer.dmnd
+
+done
+
+#abricate
+abricate --db vfdb --quiet ~/metawrap_run/bin_refinement/metawrap_50_10_bins/bin.*.fa > ~/abricate/VF.table
