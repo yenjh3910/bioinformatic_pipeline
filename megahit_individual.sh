8b1d419399874d111f7ea262544f456b0d1c0287
yenjh3910 <82366605+yenjh3910@users.noreply.github.com>
2022-05-09 20:31:48 +0800

    Update megahit_individual.sh

--- megahit_individual.sh
+++ megahit_individual.sh
@@ -3,6 +3,6 @@
 for i in $(<~/sample_list/clean_read_list)
 do
 
-megahit -t 16 -m 0.95 -1 ~/clean_read/${i}_1.fastq.gz -2 ~/clean_read/${i}_2.fastq.gz --min-contig-len 1000 -o ~/megahit/megehit_individual
+megahit -t 16 -m 0.95 -1 ~/clean_read/${i}_1.fastq.gz -2 ~/clean_read/${i}_2.fastq.gz --min-contig-len 1000 -o ~/megahit/megehit_individual/${i}_assembly
 
 done
