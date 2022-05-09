cb687cf54a8132b2caa694a12333be08675a34b5
yenjh3910 <82366605+yenjh3910@users.noreply.github.com>
2022-05-09 23:09:41 +0800

    Update prodigal_cdhit.sh

--- prodigal_cdhit.sh
+++ prodigal_cdhit.sh
@@ -6,8 +6,8 @@ mkdir ~/cdhit
 for i in {1..36}
 do
 
-prodigal -i ~/metawrap_run/bin_refinement/metawrap_50_10_bins/bin.$i.fa -o ~/prodigal/bin.$i.prodigalout -a ~/prodigal/bin.$i.prot -d ~/prodigal/bin.$i.nucl -c -p meta
-prodigal -i ~/metawrap_run/bin_refinement/metawrap_50_10_bins/bin.$i.fa -o ~/prodigal/bin.$i.gff -c -p meta -f gff
+prodigal -i ~/metawrap_run/bin_refinement/metawrap_50_10_bins/bin.$i.fa -o ~/prodigal/bin.$i.gff -a ~/prodigal/bin.$i.prot -d ~/prodigal/bin.$i.nucl -c -p meta -f gff
+gffread ~/prodigal/bin.$i.gff -T -o ~/prodigal/bin.$i.gtf
 cd-hit -i ~/prodigal/bin.$i.nucl -o ~/cdhit/bin.$i.nucl.uniq -M 0 -T 0 -l 250 -s 0.9 -c 0.9
 cd-hit -i ~/prodigal/bin.$i.prot -o ~/cdhit/bin.$i.prot.uniq -M 0 -T 0 -l 80 -c 0.9
 
