33da51d07da7d4fc75e502a9f005fd92d6c3b6d9
yenjh3910 <82366605+yenjh3910@users.noreply.github.com>
2022-05-11 23:55:44 +0800

    Update plasflow.sh

--- plasflow.sh
+++ plasflow.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+mkdir ~/PlasFlow
+
 for i in $(<~/sample_list/clean_read_list)
 do
 
-PlasFlow.py --input ~/assembly_prodigal/${i}_assembly_1000.fasta --output {i}.plasflow_predictions.tsv --threshold 0.7
+PlasFlow.py --input ~/assembly_prodigal/${i}_assembly_1000.fasta --output ~/PlasFlow/${i}.plasflow_predictions.tsv --threshold 0.7
 
 done
