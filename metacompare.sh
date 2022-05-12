653c4a1f49b66086aafe623f311ea80c841b0645
yenjh3910 <82366605+yenjh3910@users.noreply.github.com>
2022-05-12 21:41:58 +0800

    Update metacompare.sh

--- metacompare.sh
+++ metacompare.sh
@@ -4,9 +4,14 @@ cd ~/MetaCompare
 
 for i in $(<~/sample_list/clean_read_list)
 do
-mv ~/megahit/megehit_individual/${i}_assembly/final.contigs.fa ~/megahit/megehit_individual/${i}_assembly/${i}_final.contigs.fa
+#rename final contigs & move to ~/metacompare
+mv ~/megahit/megehit_individual/${i}_assembly/final.contigs.fa ${i}_final.contigs.fa
 mv ~/megahit/megehit_individual/${i}_assembly/${i}_final.contigs.fa ~/MetaCompare
-./metacmp.py -c ~/megahit/megehit_individual/${i}_assembly/final.contigs.fa -g ~/assembly_prodigal/${i}_assembly.nucl
+#move prodigal nucl to ~/metacompare
+mv ~/assembly_prodigal/${i}_assembly.nucl ~/MetaCompare
+#run metacompare
+./metacmp.py -c ${i}_final.contigs.fa -g ${i}_assembly.nucl
+#move file back
 mv ~/MetaCompare/${i}_final.contigs.fa  ~/megahit/megehit_individual/${i}_assembly
-
+mv ~/MetaCompare/${i}_assembly.nucl  ~/assembly_prodigal
 done
