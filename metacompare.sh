ecab2841f34762bcac13d35aa3c7579f54e94e39
yenjh3910 <82366605+yenjh3910@users.noreply.github.com>
2022-05-12 20:53:49 +0800

    Update metacompare.sh

--- metacompare.sh
+++ metacompare.sh
@@ -4,7 +4,9 @@ cd ~/MetaCompare
 
 for i in $(<~/sample_list/clean_read_list)
 do
-
+mv ~/megahit/megehit_individual/${i}_assembly/final.contigs.fa ~/megahit/megehit_individual/${i}_assembly/${i}_final.contigs.fa
+mv ~/megahit/megehit_individual/${i}_assembly/${i}_final.contigs.fa ~/MetaCompare
 ./metacmp.py -c ~/megahit/megehit_individual/${i}_assembly/final.contigs.fa -g ~/assembly_prodigal/${i}_assembly.nucl
+mv ~/MetaCompare/${i}_final.contigs.fa  ~/megahit/megehit_individual/${i}_assembly
 
 done
