180d825febfcb878bfdb38c72dce1152555e61b0
yenjh3910 <82366605+yenjh3910@users.noreply.github.com>
2022-08-02 21:03:26 +0800

    Update SARG_virulence_factor_diamond.sh

--- SARG_virulence_factor_diamond.sh
+++ SARG_virulence_factor_diamond.sh
@@ -10,6 +10,6 @@ do
 diamond blastx -d ~/Database/arg-oap/SARG.fasta.dmnd -q ~/cdhit/bin.$i.nucl.uniq --id 50 -p 16 -e 1e-7 -k 1 --query-cover 50 -o ~/ARGs_SARG/bin.$i.nucl.uniq.SARG.dmnd
 
 #VF detection
-diamond blastx -d ~/Database/vf.dmnd -q ~/cdhit/bin.$i.nucl.uniq --id 50 -p 16 -e 1e-7 -k 1 --query-cover 50 -o ~/ARGs_SARG/bin.$i.nucl.uniq.vf.dmnd
+diamond blastx -d ~/Database/vf.dmnd -q ~/cdhit/bin.$i.nucl.uniq --id 50 -p 16 -e 1e-7 -k 1 --query-cover 50 -o ~/virulence_factor/bin.$i.nucl.uniq.vf.dmnd
 
 done
