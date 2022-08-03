3479b103d7d263917a1972a7964d4ab02c8a7208
yenjh3910 <82366605+yenjh3910@users.noreply.github.com>
2022-08-03 16:49:56 +0800

    Update SARG_MGEs_VFs_diamond.sh

--- SARG_MGEs_VFs_diamond.sh
+++ SARG_MGEs_VFs_diamond.sh
@@ -8,12 +8,12 @@ for i in {1..129}
 do
 
 #ARG-SARG detection
-diamond blastx -d ~/Database/arg-oap/SARG_manual.fasta.dmnd -q ~/cdhit/bin.$i.nucl.uniq --id 80 -p 16 -e 1e-10 -k 1 --query-cover 70 -o ~/ARGs_SARG/bin.$i.nucl.uniq.SARG.dmnd
+diamond blastx -d ~/Database/arg-oap/SARG_manual.fasta.dmnd -q ~/cdhit/bin.$i.nucl.uniq --id 70 -p 16 -e 1e-10 -k 1 --query-cover 70 -o ~/ARGs_SARG/bin.$i.nucl.uniq.SARG.dmnd
 
 #MGE detection
-diamond blastx -d ~/Database/MGEs_database_amino.dmnd -q ~/cdhit/bin.$i.nucl.uniq --id 80 -p 16 -e 1e-10 -k 1 --query-cover 70 -o ~/MGEs_2.0/bin.$i.nucl.uniq.MGEv2.dmnd
+diamond blastx -d ~/Database/MGEs_database_amino.dmnd -q ~/cdhit/bin.$i.nucl.uniq --id 70 -p 16 -e 1e-10 -k 1 --query-cover 70 -o ~/MGEs_2.0/bin.$i.nucl.uniq.MGEv2.dmnd
 
 #VF detection
-diamond blastx -d ~/Database/vf.dmnd -q ~/cdhit/bin.$i.nucl.uniq --id 80 -p 16 -e 1e-10 -k 1 --query-cover 70 -o ~/VFs_2.0/bin.$i.nucl.uniq.vf.dmnd
+diamond blastx -d ~/Database/vf.dmnd -q ~/cdhit/bin.$i.nucl.uniq --id 70 -p 16 -e 1e-10 -k 1 --query-cover 70 -o ~/VFs_2.0/bin.$i.nucl.uniq.vf.dmnd
 
 done
