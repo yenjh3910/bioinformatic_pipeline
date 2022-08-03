5de407c22b5f1ba02db4a7f1ffeb53fd8d1e648d
yenjh3910 <82366605+yenjh3910@users.noreply.github.com>
2022-08-03 16:14:55 +0800

    Update SARG_MGEs_VFs_diamond.sh

--- SARG_MGEs_VFs_diamond.sh
+++ SARG_MGEs_VFs_diamond.sh
@@ -8,7 +8,7 @@ for i in {1..129}
 do
 
 #ARG-SARG detection
-diamond blastx -d ~/Database/arg-oap/SARG.fasta.dmnd -q ~/cdhit/bin.$i.nucl.uniq --id 80 -p 16 -e 1e-10 -k 1 --query-cover 70 -o ~/ARGs_SARG/bin.$i.nucl.uniq.SARG.dmnd
+diamond blastx -d ~/Database/arg-oap/SARG_manual.fasta.dmnd -q ~/cdhit/bin.$i.nucl.uniq --id 80 -p 16 -e 1e-10 -k 1 --query-cover 70 -o ~/ARGs_SARG/bin.$i.nucl.uniq.SARG.dmnd
 
 #MGE detection
 diamond blastx -d ~/Database/MGEs_database_amino.dmnd -q ~/cdhit/bin.$i.nucl.uniq --id 80 -p 16 -e 1e-10 -k 1 --query-cover 70 -o ~/MGEs_2.0/bin.$i.nucl.uniq.MGEv2.dmnd
