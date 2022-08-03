46bdfeba721e400176f3d38ce5116da663809a3e
yenjh3910 <82366605+yenjh3910@users.noreply.github.com>
2022-08-03 15:43:21 +0800

    Update and rename SARG_virulence_factor_diamond.sh to SARG_MGEs_VFs_diamond.sh

---
+++ SARG_MGEs_VFs_diamond.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+mkdir ~/ARGs_SARG
+mkdir ~/MGEs_2.0
+mkdir ~/VFs_2.0
+
+for i in {1..36}
+do
+
+#ARG-SARG detection
+diamond blastx -d ~/Database/arg-oap/SARG.fasta.dmnd -q ~/cdhit/bin.$i.nucl.uniq --id 50 -p 16 -e 1e-7 -k 1 --query-cover 50 -o ~/ARGs_SARG/bin.$i.nucl.uniq.SARG.dmnd
+
+#MGE detection
+diamond blastx -d ~/Database/MGEs_database_amino.dmnd -q ~/cdhit/bin.$i.nucl.uniq --id 50 -p 16 -e 1e-7 -k 1 --query-cover 50 -o ~/MGEs_2.0/bin.$i.nucl.uniq.MGEv2.dmnd
+
+#VF detection
+diamond blastx -d ~/Database/vf.dmnd -q ~/cdhit/bin.$i.nucl.uniq --id 50 -p 16 -e 1e-7 -k 1 --query-cover 50 -o ~/VFs_2.0/bin.$i.nucl.uniq.vf.dmnd
+
+done
