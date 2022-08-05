3b44079863f2e3d765e6b9a73bfe965967ad2602
yenjh3910 <82366605+yenjh3910@users.noreply.github.com>
2022-08-05 11:28:59 +0800

    Update SARG_MGEs_VFs_diamond.sh

--- SARG_MGEs_VFs_diamond.sh
+++ SARG_MGEs_VFs_diamond.sh
@@ -3,6 +3,7 @@
 mkdir ~/ARGs_SARG
 mkdir ~/MGEs_2.0
 mkdir ~/VFs_2.0
+mkdir~/MRGs_2.0
 
 for i in {1..129}
 do
@@ -15,5 +16,8 @@ diamond blastx -d ~/Database/MGEs_database_amino.dmnd -q ~/cdhit/bin.$i.nucl.uni
 
 #VF detection
 diamond blastx -d ~/Database/vf.dmnd -q ~/cdhit/bin.$i.nucl.uniq --id 70 -p 16 -e 1e-10 -k 1 --query-cover 70 -o ~/VFs_2.0/bin.$i.nucl.uniq.vf.dmnd
+
+#MRG detection
+diamond blastx -d ~/Database/BacMet2_EXP.dmnd -q ~/cdhit/bin.$i.nucl.uniq --id 70 -p 16 -e 1e-10 -k 1 --query-cover 70 -o ~/MRGs_2.0/bin.$i.nucl.uniq.MRG.dmnd
 
 done
