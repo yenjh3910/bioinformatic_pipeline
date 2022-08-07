3f01f870883567420d4d95b691c5d805d2a26205
yenjh3910 <82366605+yenjh3910@users.noreply.github.com>
2022-08-07 14:05:28 +0800

    Update individual_assembly_diamond.sh

--- individual_assembly_diamond.sh
+++ individual_assembly_diamond.sh
@@ -15,7 +15,7 @@ cd-hit -i ~/assembly_prodigal/${i}_assembly.nucl -o ~/cdhit_individual_assembly/
 cd-hit -i ~/assembly_prodigal/${i}_assembly.prot -o ~/cdhit_individual_assembly/${i}.prot.uniq -M 0 -T 0 -l 80 -c 0.9
 
 #SARG detection
-diamond blastx -d ~/Database/arg-oap/SARG_manual.fasta.dmnd -q ~/cdhit_individual_assembly/${i}.nucl.uniq --id 70 -p 16 -e 1e-10 -k 1 --query-cover 70 -o ~/individual_assembly_diamond/SARG/${i}_assembly.nucl.SARG.dmnd
+diamond blastx -d ~/Database/arg-oap/SARG_manual.dmnd -q ~/cdhit_individual_assembly/${i}.nucl.uniq --id 70 -p 16 -e 1e-10 -k 1 --query-cover 70 -o ~/individual_assembly_diamond/SARG/${i}_assembly.nucl.SARG.dmnd
 
 #MGE detection
 diamond blastx -d ~/Database/MGEs_database_amino.dmnd -q ~/cdhit_individual_assembly/${i}.nucl.uniq --id 70 -p 16 -e 1e-10 -k 1 --query-cover 70 -o ~/individual_assembly_diamond/MGE/${i}_assembly.nucl.MGE.dmnd
