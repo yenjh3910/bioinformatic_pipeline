b67789e9fc3b72ab01e3dc37c089ff0f02e016f0
yenjh3910 <82366605+yenjh3910@users.noreply.github.com>
2022-08-08 10:37:25 +0800

    Update contigs_kraken2.sh

--- contigs_kraken2.sh
+++ contigs_kraken2.sh
@@ -6,6 +6,6 @@ for i in $(<~/sample_list/sample_type_list)
 do
 
 #kraken2
-kraken2 --db ~/kraken_db --threads 16 --report ~/contigs_kraken2/${i}_contigs_kraken2.report --output ~/contigs_kraken2/${i}_contigs_kraken2.output ~/megahit/megehit_individual/${i}_assembly/${i}_final.contigs.fa --use-names
+kraken2 --db ~/Dkraken_db --threads 16 --report ~/contigs_kraken2/${i}_contigs_kraken2.report --output ~/contigs_kraken2/${i}_contigs_kraken2.output ~/megahit/megehit_individual/${i}_assembly/${i}_final.contigs.fa --use-names
 
 done
